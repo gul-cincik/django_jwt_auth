3c8b320a144a3c4b4c908fb87d39a20d7517147e
gul-cincik <cnckgl80@gmail.com>
2023-08-21 14:55:38 +0300

    tool update

--- userTool/functions.py
+++ userTool/functions.py
@@ -3,18 +3,48 @@ import requests
 BASE_URL = 'http://127.0.0.1:8000/user/'
 
 def register(username, password):
+    """
+    Registers a new user.
+
+    Args:
+        username (str): Username of the user.
+        password (str): Password of the user.
+
+    Returns:
+        dict: username 
+    """
     url = BASE_URL + 'register/'
     data = {'username': username, 'password': password}
     response = requests.post(url, json=data)
     return response.json()
 
 def login(username, password):
+    """
+    Logs in a user and obtains a JWT token.
+
+    Args:
+        username (str): Username of the user.
+        password (str): Password of the user.
+
+    Returns:
+        dict: JSON response containing JWT token.
+    """
     url = BASE_URL + 'login/'
     data = {'username': username, 'password': password}
     response = requests.post(url, json=data)
     return response.json()
 
 def get_breweries(token, query=None):
+    """
+    Fetches brewery data using a JWT token.
+
+    Args:
+        token (str): JWT token obtained after login.
+        query (str, optional): Query parameter for filtering breweries. Defaults to None.
+
+    Returns:
+        dict: JSON response containing brewery data.
+    """
     url = BASE_URL + f'breweries/?query={query}' if query else BASE_URL + 'breweries/'
     headers = {'Authorization': f'Bearer {token}'}
     response = requests.get(url, headers=headers)
--- userTool/main.py
+++ userTool/main.py
@@ -1,28 +1,29 @@
 from functions import register, login, get_breweries
 
+while True:
+    option = input("Select an option (1=Register, 2=Login, 3=Breweries, 4=Exit): ")
 
-option = input("Select an option (1=Register, 2=Login, 3=Breweries): ")
-    
-if option == '1':
-    username = input("Enter username: ")
-    password = input("Enter password: ")
-    response = register(username, password)
-    print(response)
+    if option == '1':
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+        response = register(username, password)
+        print(response)
 
-elif option == '2':
-    username = input("Enter username: ")
-    password = input("Enter password: ")
-    response = login(username, password)
-    print(response)
+    elif option == '2':
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+        response = login(username, password)
+        print(response)
 
-elif option == '3':
-    token = input("Enter your JWT token: ")
-    query = input("Enter query parameter (or press Enter for no query): ")
-    response = get_breweries(token, query)
-    print(response)
+    elif option == '3':
+        token = input("Enter your JWT token: ")
+        query = input("Enter query parameter (or press Enter for no query): ")
+        response = get_breweries(token, query)
+        print(response)
 
-else:
-    print("Invalid option.")
+    elif option == '4':
+        print("Exiting the program.")
+        break
 
-
-
+    else:
+        print("Invalid option. Please select a valid option.")
